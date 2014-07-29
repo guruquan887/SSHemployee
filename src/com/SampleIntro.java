d62d7e88f6fff04e9c44a587870c95087e1d7743
姜泉 <595121869@qq.com>
2014-07-29 19:02:54 +0800

    Java链接数据库MySQL的代码
    Java链接数据库MySQL的代码

---
+++ src/com/SampleIntro.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.sql.*;
+
+public class SampleIntro
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+      Connection conn;
+      Statement stmt;
+      ResultSet res;
+      //加载Connector/J驱动
+     //这一句也可写为：Class.forName("com.mysql.jdbc.Driver");
+      Class.forName("com.mysql.jdbc.Driver").newInstance();
+      //建立到MySQL的连接
+     conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/guruquan",
+                                         "root", "root");
+      //执行SQL语句
+     stmt = conn.createStatement();
+      res = stmt.executeQuery("select * from tb_shark_config");
+      //处理结果集
+     while (res.next())
+      {
+        String id = res.getString("id");
+        System.out.println(id);
+      }
+      res.close();
+
+    }
+    catch (Exception ex)
+    {
+      System.out.println("Error : " + ex.toString()+"链接数据库错误。database connection error！");
+    }
+  }
+}
